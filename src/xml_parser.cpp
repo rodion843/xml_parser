28e33ccac98ba92059e4d9d87f3b07eea281e3a7
Rodion <rodion843@gmail.com>
2022-02-14 10:38:54 +0300

    able to parse header

--- src/xml_parser.cpp
+++ src/xml_parser.cpp
@@ -1,23 +1,45 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <fstream>
+//<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 
+struct xml_header
+{
+  std::string version;
+  std::string encoding;
+  bool standalone;
+};
+
+auto parseHeader(const auto &line)
+{
+  std::stringstream ss{line};
+  char dump[100];
+  ss.getline(dump, 100, '\"');
+
+  char version[10];
+  ss.getline(version, 10, '\"');
+
+  ss.getline(dump, 100, '\"');
+  char encoding[10];
+  ss.getline(encoding, 10, '\"');
+
+  ss.getline(dump, 100, '\"');
+  char standalone[10];
+  ss.getline(standalone, 10, '\"');
+
+  xml_header hdr {{version}, 
+	          {encoding}, 
+		  std::string{standalone} == std::string{"no"}
+                 };
+}
 int main()
 {
   std::string path {"./sample.xml"};
   std::fstream fs(path);
   if(!fs.is_open()) std::cerr << "could not open file " << path;
-  char output[200];
-  while(fs.getline(output, 200))
-  {
-	  std::cout << output << '\n';
-  }
-  getheader();
-  auto key {getNextKey(fs)};
-  while (key)
-  {
-    
-
-  }
+  std::string str;
+  getline(fs, str);
+  parseHeader(str);
   return 0;
 }
